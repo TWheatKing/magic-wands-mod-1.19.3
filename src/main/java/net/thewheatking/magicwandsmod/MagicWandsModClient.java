f3312d31ebbe66d99bea9bfeef1da27e00e7dc1f
TWheatKing <andrew.wheatcroft2@gmail.com>
2023-02-23 16:38:29 +0000

    Added an important code in order to run the client

---
+++ src/main/java/net/thewheatking/magicwandsmod/MagicWandsModClient.java
@@ -0,0 +1,10 @@
+package net.thewheatking.magicwandsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicWandsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
