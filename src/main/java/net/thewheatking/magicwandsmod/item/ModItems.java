9c4add5351b5ebd40d67b27d2351bc6619830287
TWheatKing <andrew.wheatcroft2@gmail.com>
2023-02-23 17:19:44 +0000

    Added code to make the first item Magical Wood :) Next is to add the .png file of this item :D

---
+++ src/main/java/net/thewheatking/magicwandsmod/item/ModItems.java
@@ -0,0 +1,33 @@
+package net.thewheatking.magicwandsmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.thewheatking.magicwandsmod.MagicWandsMod;
+
+public class ModItems {
+    public static final Item MAGICAL_WOOD = registerItem("magical wood",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MagicWandsMod.MOD_ID, name), item);
+    }
+
+    public static void  addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.INGREDIENTS, MAGICAL_WOOD);
+    }
+
+    public static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+    public static void registerModItems() {
+        MagicWandsMod.LOGGER.info("Registering Mod Items for " + MagicWandsMod.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
